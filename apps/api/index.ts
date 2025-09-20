9635eae83721b84dcc7b6c683fbb3333de53b9c6
rohanshrma222 <hackerviruse04@gmail.com>
2025-09-20 17:05:41 +0530

    added tests

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import { prismaClient } from "store/client"
+
 const app = express();
+app.use(express.json());
 
-app.post("/website",(req,res) =>{
-
+app.post("/website",async(req,res) =>{
+    const website = await prismaClient.website.create({
+        data:{
+            url: req.body.url,
+            timeAdded: new Date()
+        }
+    })
+    
+    res.json({
+        id: website.id                                          
+    })
 });
 
 app.get("/status/:websiteId",(req,res)=>{
---
+++ apps/tests/website.test.ts
@@ -0,0 +1,25 @@
+import {describe,it,expect} from "bun:test";
+import axios from "axios"
+
+let BASE_URL = "http://localhost: 3001";
+
+describe("Website gets created", () =>{
+    it("Website not created if url is not present", async ()=>{
+        try{
+            await axios.post(`${BASE_URL}/website`, {
+
+            });
+            expect(false, "Website created when it shouldn't");
+        }
+        catch(e){
+
+        }
+    })
+
+    it("Website not created if url is not present", async ()=>{
+        const response = await axios.post(`${BASE_URL}/website`,{
+            url: "https:google.com"
+        })
+            expect(response.data.id).not.toBeNull;
+    })
+})
