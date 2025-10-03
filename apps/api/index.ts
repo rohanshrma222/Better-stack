6d9cb86af3f93396dd0587391564ffd21d165a3d
rohanshrma222 <hackerviruse04@gmail.com>
2025-10-03 23:46:59 +0530

    added zod vali,wrote some auth logic

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { prismaClient } from "store/client"
+import { AuthInput } from "./types";
+import  jwt  from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
@@ -19,6 +21,54 @@ app.post("/website",async(req,res) =>{
 });
 
 app.get("/status/:websiteId",(req,res)=>{
+
+})
+
+app.post("/user/signup",async(req,res)=>{
+    const data = AuthInput.safeParse(req.body.data);
+    if(!data.success){
+        res.status(403).send("");
+        return;
+    }
+    try{    
+        let user = await prismaClient.user.create({
+            data: {
+            username: data.data.username,
+            password: data.data.password
+            }
+        })
+        res.json({
+            id: user.id
+        })
+    } catch(e){
+        res.status(403).send("");
+    }
+})
+
+app.post("/user/signin",async(req,res)=>{
+    const data = AuthInput.safeParse(req.body.data);
+    if(!data.success){
+        res.status(403).send("");
+        return;
+    }
+    let user = await prismaClient.user.findFirst({
+        where:{
+            username: data.data.username
+        }
+    })
+
+    if(user?.password !== data.data.password){
+        res.status(403).send("");
+        return;
+    }
+
+    let token = jwt.sign({
+        sub: user.id
+    }, process.env.JWT_SECRET!)
+
+    res.json({
+        jwt: token
+    })
 
 })
 
