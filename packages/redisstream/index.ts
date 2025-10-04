3c319d2fa5753a3bcd1dc60c7363536d4cca6148
rohanshrma222 <hackerviruse04@gmail.com>
2025-10-05 01:43:01 +0530

    added worker logic

--- packages/redisstream/index.ts
+++ packages/redisstream/index.ts
@@ -4,29 +4,57 @@ const client = await createClient()
   .on("error", (err) => console.log("Redis Client Error", err))
   .connect();
 
-  type WebsiteEvent = {url: string, id: string}
+type WebsiteEvent = {url: string, id: string}
+type MessageType = {
+    id: string,
+    message: {
+        url: string,
+        id: string
+    }
+    //@ts-ignore
+}
 
-     async function xAdd({url, id}: WebsiteEvent){
-     await client.xAdd(
-        'betteruptime:website', '*' , {
+const STREAM_NAME = "betteruptime:website";
+
+async function xAdd({url, id}: WebsiteEvent) {
+    await client.xAdd(
+        STREAM_NAME, '*', {
             url,
             id
         }
     );
 }
 
-export async function xAddBulk(websites: WebsiteEvent[]): Promise<void> {
-    if (!websites || websites.length === 0) {
-        console.warn('No websites provided to xAddBulk');
-        return;
+export async function xAddBulk(websites: WebsiteEvent[]) {
+    for (let i = 0; i < websites.length; i++) {
+        await xAdd({
+            url: websites[i].url,
+            id: websites[i].id
+        })
     }
+}
+
+export async function xReadGroup(consumerGroup: string, workerId: string): Promise<MessageType[] | undefined> {
     
-    for (const website of websites) {
-        if (website?.url && website?.id) {
-            await xAdd({
-                url: website.url,
-                id: website.id
-            });
+    const res = await client.xReadGroup(
+        consumerGroup, workerId, {
+            key: STREAM_NAME,
+            id: '>'
+        }, {
+        'COUNT': 5
         }
-    }
+    );
+
+    //@ts-ignore
+    let messages: MessageType[] | undefined = res?.[0]?.messages;
+
+    return messages;
+}
+
+async function xAck(consumerGroup: string, eventId: string) {
+    await client.xAck(STREAM_NAME, consumerGroup, eventId)
+}
+
+export async function xAckBulk(consumerGroup: string, eventIds: string[]) {
+    eventIds.map(eventId => xAck(consumerGroup, eventId));
 }
